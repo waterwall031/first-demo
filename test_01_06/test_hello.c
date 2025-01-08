cded43ff296f69a9b2d3a34b21742e34312b1b56
waterwall <1826602177@qq.com>
2025-01-08 22:06:13 +0800

    first save

---
+++ test_01_06/test_hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "addi.h" // quote user's header file
+
+int main()
+{
+    printf("Hello VSCode!\n");
+    int a = 3, b = 2;
+    int c = addi(a, b);
+    printf("%d", a);
+    if (a>0)
+    {
+        printf("%d", a);
+    }
+
+    return 0;
+}
